79ab789ed3466051eb4fbfec358cdc78905b4705
camilocro <camilorodriguezandre@gmail.com>
2025-11-22 18:14:41 -0400

    Feat: Implement TeamCar repository pattern

--- Repositories/ITeamCarRepository.cs
+++ Repositories/ITeamCarRepository.cs
@@ -1,6 +1,13 @@
-﻿namespace Proyecto_FInal_Grupo_1.Repositories
+﻿using Proyecto_FInal_Grupo_1.Models;
+
+namespace Proyecto_FInal_Grupo_1.Repositories
 {
-    public class ITeamCarRepository
+    public interface ITeamCarRepository
     {
+        Task<IEnumerable<TeamCar>> GetAll();
+        Task<TeamCar?> GetOne(Guid id);
+        Task Add(TeamCar teamCar);
+        Task Update(TeamCar teamCar);
+        Task Delete(TeamCar teamCar);
     }
 }
--- Repositories/TeamCarRepository.cs
+++ Repositories/TeamCarRepository.cs
@@ -1,6 +1,43 @@
-﻿namespace Proyecto_FInal_Grupo_1.Repositories
+﻿using Microsoft.EntityFrameworkCore;
+using Proyecto_FInal_Grupo_1.Data;
+using Proyecto_FInal_Grupo_1.Models;
+
+namespace Proyecto_FInal_Grupo_1.Repositories
 {
-    public class TeamCarRepository
+    public class TeamCarRepository : ITeamCarRepository
     {
+        private readonly AppDbContext _db;
+        public TeamCarRepository(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task Add(TeamCar teamCar)
+        {
+            await _db.TeamCars.AddAsync(teamCar);
+            await _db.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<TeamCar>> GetAll()
+        {
+            return await _db.TeamCars.ToListAsync();
+        }
+
+        public async Task<TeamCar?> GetOne(Guid id)
+        {
+            return await _db.TeamCars.FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        public async Task Update(TeamCar teamCar)
+        {
+            _db.TeamCars.Update(teamCar);
+            await _db.SaveChangesAsync();
+        }
+
+        public async Task Delete(TeamCar teamCar)
+        {
+            _db.TeamCars.Remove(teamCar);
+            await _db.SaveChangesAsync();
+        }
     }
 }

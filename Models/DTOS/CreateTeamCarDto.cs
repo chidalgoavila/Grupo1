fcd244b48dea26c489c872eb39ec4e74c6d2b2f3
camilocro <camilorodriguezandre@gmail.com>
2025-11-22 18:13:36 -0400

    Feat: Add DTOs for TeamCar

--- Models/DTOS/CreateTeamCarDto.cs
+++ Models/DTOS/CreateTeamCarDto.cs
@@ -1,6 +1,14 @@
-﻿namespace Proyecto_FInal_Grupo_1.Models.DTOS
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Proyecto_FInal_Grupo_1.Models.DTOS
 {
-    public class CreateTeamCarDto
+    public record CreateTeamCarDto
     {
+        [Required]
+        public string Model { get; set; }
+        [Required]
+        public string TeamName { get; set; }
+        public string Engine { get; set; }
+        public int Year { get; set; }
     }
 }

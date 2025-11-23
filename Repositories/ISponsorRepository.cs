a6ab1dbe41c10b261a1f1bf875f0a2d1ff888c00
isa2206 <hidalgoavilaisabel@gmail.com>
2025-11-23 12:09:04 -0400

    Feat:SponsorRepository

--- Repositories/ISponsorRepository.cs
+++ Repositories/ISponsorRepository.cs
@@ -1,6 +1,13 @@
-﻿namespace Proyecto_FInal_Grupo_1.Repositories
+﻿using Proyecto_FInal_Grupo_1.Models;
+
+namespace Proyecto_FInal_Grupo_1.Repositories
 {
-    public class ISponsorRepository
+    public interface ISponsorRepository
     {
+        Task<IEnumerable<Sponsor>> GetAll();
+        Task<Sponsor?> GetOne(Guid id);
+        Task Add(Sponsor sponsor);
+        Task Update(Sponsor sponsor);
+        Task Delete(Sponsor sponsor);
     }
 }
--- Repositories/SponsorRepository.cs
+++ Repositories/SponsorRepository.cs
@@ -1,6 +1,46 @@
-﻿namespace Proyecto_FInal_Grupo_1.Repositories
+﻿using Microsoft.EntityFrameworkCore;
+using Proyecto_FInal_Grupo_1.Data;
+using Proyecto_FInal_Grupo_1.Models;
+
+namespace Proyecto_FInal_Grupo_1.Repositories
 {
-    public class SponsorRepository
+    public class SponsorRepository : ISponsorRepository
     {
+        private readonly AppDbContext _db;
+        public SponsorRepository(AppDbContext db)
+        {
+            _db = db;
+        }
+        public async Task<IEnumerable<Sponsor>> GetAll()
+        {
+            return await _db.Sponsors
+                .Include(s => s.Drivers)         
+                .Include(s => s.CarSponsors)     
+                .ThenInclude(cs => cs.TeamCar)    
+                .ToListAsync();
+        }
+        public async Task<Sponsor?> GetOne(Guid id)
+        {
+            return await _db.Sponsors
+                .Include(s => s.Drivers)
+                .Include(s => s.CarSponsors)
+                .ThenInclude(cs => cs.TeamCar)
+                .FirstOrDefaultAsync(x => x.Id == id);
+        }
+        public async Task Add(Sponsor sponsor)
+        {
+            await _db.Sponsors.AddAsync(sponsor);
+            await _db.SaveChangesAsync();
+        }
+        public async Task Update(Sponsor sponsor)
+        {
+            _db.Sponsors.Update(sponsor);
+            await _db.SaveChangesAsync();
+        }
+        public async Task Delete(Sponsor sponsor)
+        {
+            _db.Sponsors.Remove(sponsor);
+            await _db.SaveChangesAsync();
+        }
     }
 }

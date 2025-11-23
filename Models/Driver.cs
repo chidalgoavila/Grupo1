43c5446edb6fb1f1423b55837fd620537ffde44d
Fabio Garcia Meza <fabio.garciameza@ucb.edu.bo>
2025-11-23 15:37:19 -0400

    feat(Models): Adding Entity Driver.cs with atributes and FKs

--- Models/Driver.cs
+++ Models/Driver.cs
@@ -1,6 +1,27 @@
-﻿namespace Proyecto_FInal_Grupo_1.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Proyecto_FInal_Grupo_1.Models
 {
     public class Driver
     {
+        public Guid Id { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        public int Number { get; set; }
+
+        [Required]
+        public string Nationality { get; set; }
+
+        public Guid TeamCarId { get; set; }
+        public TeamCar? TeamCar { get; set; }
+
+        public Guid SponsorId { get; set; }
+        public Sponsor? Sponsor { get; set; }
     }
 }

d72c8de49cf3e924dc1186fddef08776637860b8
Fabio Garcia Meza <fabio.garciameza@ucb.edu.bo>
2025-11-23 15:40:05 -0400

    feat(Repositories): Adding Driver Interface

--- Repositories/IDriverRepository.cs
+++ Repositories/IDriverRepository.cs
@@ -1,6 +1,13 @@
-﻿namespace Proyecto_FInal_Grupo_1.Repositories
+﻿using Proyecto_FInal_Grupo_1.Models;
+
+namespace Proyecto_FInal_Grupo_1.Repositories
 {
-    public class IDriverRepository
+    public interface IDriverRepository
     {
+        Task<IEnumerable<Driver>> GetAll();
+        Task<Driver?> GetOne(Guid id);
+        Task Add(Driver driver);
+        Task Update(Driver driver);
+        Task Delete(Driver driver);
     }
 }

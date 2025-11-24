c0f092351dbbf79d234355acfc99e6bdca7ebb79
camilocro <camilorodriguezandre@gmail.com>
2025-11-24 14:17:24 -0400

    Feat: Implement UserRepository logic

--- Repositories/UserRepository.cs
+++ Repositories/UserRepository.cs
@@ -1,6 +1,39 @@
-﻿namespace Proyecto_FInal_Grupo_1.Repositories
+﻿using Microsoft.EntityFrameworkCore;
+using Proyecto_FInal_Grupo_1.Data;
+using Proyecto_FInal_Grupo_1.Models;
+
+namespace Proyecto_FInal_Grupo_1.Repositories
 {
-    public class UserRepository
+    public class UserRepository : IUserRepository
     {
+        private readonly AppDbContext _context;
+
+        public UserRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<User?> GetByEmail(string email)
+        {
+            return await _context.Users.FirstOrDefaultAsync(u => u.Email == email);
+        }
+
+        public async Task<User?> GetByRefreshToken(string refreshToken)
+        {
+            return await _context.Users.FirstOrDefaultAsync(u => u.RefreshToken == refreshToken);
+        }
+
+        public async Task<User> Create(User user)
+        {
+            await _context.Users.AddAsync(user);
+            await _context.SaveChangesAsync();
+            return user;
+        }
+
+        public async Task Update(User user)
+        {
+            _context.Users.Update(user);
+            await _context.SaveChangesAsync();
+        }
     }
 }
